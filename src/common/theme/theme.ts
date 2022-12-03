93ef0e33b13997862298327ffde38e5e0ff65cbf
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2022-12-03 21:33:53 +0100

    update theme

--- src/common/theme/theme.ts
+++ src/common/theme/theme.ts
@@ -1,5 +1,15 @@
 import { createTheme } from '@mui/material/styles';
+import { purple } from '@mui/material/colors';
 
-const theme = createTheme({});
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#9c27b0'
+    },
+    secondary: {
+      main: '#f50057'
+    }
+  }
+});
 
 export default theme;
