5f12c33a232b2411ab81aa1a75ed9f0a28dcbc68
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2022-12-04 19:36:23 +0100

    update custom osd-controls

--- src/common/components/Viewer/Controls.tsx
+++ src/common/components/Viewer/Controls.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Stack from '@mui/material/Stack';
+import Tooltip from '@mui/material/Tooltip';
 import IconButton from '@mui/material/IconButton';
 import ZoomInIcon from '@mui/icons-material/ZoomIn';
 import ZoomOutIcon from '@mui/icons-material/ZoomOut';
 import CenterFocusStrongIcon from '@mui/icons-material/CenterFocusStrong';
 import CropFreeIcon from '@mui/icons-material/CropFree';
-import Stack from '@mui/material/Stack';
 
 const Controls = () => {
   return (
@@ -21,18 +22,26 @@ const Controls = () => {
         }
       }}
     >
-      <IconButton id="osd-zoom-in" aria-label="zoom in" color="primary">
-        <ZoomInIcon />
-      </IconButton>
-      <IconButton id="osd-zoom-out" aria-label="zoom oout" color="primary">
-        <ZoomOutIcon />
-      </IconButton>
-      <IconButton id="osd-home" aria-label="home" color="primary">
-        <CenterFocusStrongIcon />
-      </IconButton>
-      <IconButton id="osd-fulls" aria-label="fulls" color="primary">
-        <CropFreeIcon />
-      </IconButton>
+      <Tooltip title="Increase zoom level" placement="left">
+        <IconButton id="osd-zoom-in" color="primary">
+          <ZoomInIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Decrease zoom level" placement="left">
+        <IconButton id="osd-zoom-out" color="primary">
+          <ZoomOutIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Center" placement="left">
+        <IconButton id="osd-home" color="primary">
+          <CenterFocusStrongIcon />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Fullscreen" placement="left">
+        <IconButton id="osd-fulls" color="primary">
+          <CropFreeIcon />
+        </IconButton>
+      </Tooltip>
     </Stack>
   );
 };
