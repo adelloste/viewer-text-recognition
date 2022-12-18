3b92c8c8259380a887ca86530b0726c1202c48ff
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2022-12-18 13:12:49 +0100

    update appbar

--- src/common/components/Header/Header.tsx
+++ src/common/components/Header/Header.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+const PAGES = [{ id: 0, name: 'Library', url: '/main/library' }];
 
 const Header = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <AppBar elevation={0} position="fixed">
@@ -15,13 +22,23 @@ const Header = () => {
               noWrap
               sx={{
                 mr: 2,
-                flexGrow: 1,
                 fontWeight: 700,
                 letterSpacing: '.3rem'
               }}
             >
               Matrices
             </Typography>
+            <Box sx={{ flexGrow: 1, display: 'flex' }}>
+              {PAGES.map(({ id, name, url }) => (
+                <Button
+                  key={id}
+                  onClick={() => navigate(url)}
+                  sx={{ my: 2, color: 'white', display: 'block' }}
+                >
+                  {name}
+                </Button>
+              ))}
+            </Box>
           </Toolbar>
         </Container>
       </AppBar>
