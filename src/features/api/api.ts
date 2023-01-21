cb1b7c029ce853035235b6799f7bf7fbdf1afbac
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2023-01-21 17:26:45 +0100

    add envs

--- src/features/api/api.ts
+++ src/features/api/api.ts
@@ -2,6 +2,6 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
   reducerPath: 'api',
-  baseQuery: fetchBaseQuery({ baseUrl: '/api/v1/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: `${process.env.REACT_APP_BASE_URL}/api/v1/` }),
   endpoints: () => ({})
 });
