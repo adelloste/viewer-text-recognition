bcb2db3df7a16042c4ff50da3ab201daf8c43975
Alessandro Dell'Oste <alessandro.delloste1@gmail.com>
2023-01-21 17:29:49 +0100

    add cors

--- dev-server/src/config.js
+++ dev-server/src/config.js
@@ -4,6 +4,5 @@ const root = path.resolve('.');
 export const assets = root + '/assets';
 
 export const config = {
-  port: 8080,
   delay: 200
 };
--- dev-server/src/index.js
+++ dev-server/src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 import morgan from 'morgan';
 import bodyParser from 'body-parser';
 import { config } from './config';
@@ -7,8 +8,11 @@ import { errorHandler } from './middlewares/error-handler';
 import { libraryRouter } from './routers/library';
 import { transcriptionRouter } from './routers/transcription';
 
+const PORT = 8080;
+
 // create express server
 const app = express();
+app.use(cors());
 // parse body request as json
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +26,6 @@ app.use('/api/v1/transcription', transcriptionRouter);
 // set error handler
 app.use(errorHandler);
 
-app.listen(config.port, () => {
-  console.log('server running on port 8080');
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
 });
